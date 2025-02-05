073525a6b4ee687073c42445f31a7ce6f68bc3d1
Michael <michalluczak4a@gmail.com>
2025-02-05 15:46:00 +0100

    fixes

--- economy-gui/src/main/java/pl/psi/gui/EconomyShopLoader.java
+++ economy-gui/src/main/java/pl/psi/gui/EconomyShopLoader.java
@@ -3,24 +3,27 @@ package pl.psi.gui;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import pl.psi.EconomyEngine;
-import pl.psi.building.model.UpgradableBuilding;
+import pl.psi.building.EconomyBuildingFacade;
+import pl.psi.building.factory.EconomyBuildingAbstractFactory;
+import pl.psi.building.town.Town;
+import pl.psi.gui.shop.building.EconomyBuildingShopController;
 import pl.psi.hero.EconomyHero;
 
 import java.io.IOException;
 
 public class EconomyShopLoader {
-    public void openShop(EconomyHero hero, UpgradableBuilding aBuilding) {
+    public void openShop(EconomyHero hero, Town aTown) {
 
         try {
 
             final FXMLLoader loader = new FXMLLoader();
             loader.setLocation(getClass().getClassLoader()
-                    .getResource("fxml/eco.fxml"));
-            loader.setController(new EcoController(aBuilding, new EconomyEngine(hero)));
+                    .getResource("fxml/eco-building-shop.fxml"));
 
             final Scene scene = new Scene(loader.load());
             Stage aStage = new Stage();
+            EconomyBuildingAbstractFactory factory = new EconomyBuildingAbstractFactory();
+            loader.setController(new EconomyBuildingShopController(new EconomyBuildingFacade(factory), hero, aTown, aStage));
             aStage.setScene(scene);
             aStage.setX(5);
             aStage.setY(5);
